4b950708ca78edb3b5a38ba0230f165fd16f392e
chaerin98 <47683552+chaerin98@users.noreply.github.com>
2022-10-03 15:54:20 +0900

    Update 2 on GitHub branch Only

--- main.py
+++ main.py
@@ -21,3 +21,6 @@ print("Hello World Update on GitHub branch")
 
 #adding new item in masterbranch
 print("Hello World Updates on Local Master Branch")
+
+#update in main branch of GitHub
+print("Hello World Update 2 on GitHub branch Only")
